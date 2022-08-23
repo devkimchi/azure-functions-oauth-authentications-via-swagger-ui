bb1b816a67d759d9e67ded4906b9a90e6508fb0e
Justin Yoo <justin.yoojh@gmail.com>
2022-08-24 00:14:25 +0900

    Update Easy Auth trigger

--- FunctionApp/HttpTriggers/OAuthEasyAuthFlowHttpTrigger.cs
+++ FunctionApp/HttpTriggers/OAuthEasyAuthFlowHttpTrigger.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -18,7 +17,7 @@ namespace FunctionApp.HttpTriggers
     public static class OAuthEasyAuthFlowHttpTrigger
     {
         [FunctionName(nameof(OAuthEasyAuthFlowHttpTrigger))]
-        [OpenApiOperation(operationId: "oauth.flows.easyauth", tags: new[] { "oauth" }, Summary = "OAuth easy auth flows", Description = "This shows the OAuth easy auth flows", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiOperation(operationId: "oauth.flows.easyauth", tags: new[] { "oauth" }, Summary = "OAuth easy auth flows - MUST be deployed to Azure", Description = "This shows the OAuth easy auth flows. To use this feature, this function app MUST be deployed to Azure.", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(IEnumerable<string>), Summary = "successful operation", Description = "successful operation")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = null)] HttpRequest req,
@@ -26,10 +25,7 @@ namespace FunctionApp.HttpTriggers
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var headers = req.Headers.ToDictionary(p => p.Key, p => (string)p.Value);
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadJwtToken(headers["Authorization"].Split(' ').Last());
-            var claims = token.Claims.Select(p => p.ToString());
+            var claims = req.HttpContext.User.Claims.Select(p => p.ToString());
 
             var result = new OkObjectResult(claims);
 
